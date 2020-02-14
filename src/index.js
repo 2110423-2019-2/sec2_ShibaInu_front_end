0a074f459dc099542d464c48bb75f841bc9d9b2a
Watcharit Tuntayakul <watcharit.new@gmail.com>
2020-02-15 00:17:55 +0700

    merge with profile

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import HomeClient from './HomeClient';
 import 'bootstrap/dist/css/bootstrap.css';
 import Profile from './Profile'
 
-ReactDOM.render(<Profile />, document.getElementById('root'));
+ReactDOM.render(<HomeClient />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
