1ba1904b89eaf345623e9c5b211d6f9454bef4f4
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-02-19 00:24:09 +0700

    connect SignIn and SignUp to backend via axios

--- src/SignIn.js
+++ src/SignIn.js
@@ -2,24 +2,49 @@ import React from 'react';
 import NavBar from "./NavBar";
 import logo from './material/Logo.png';
 import './SignInSignUp.css';
+import axios from 'axios';
 
 class SignIn extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            loginData: { username: '', password: '' },
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit = (event) => {
-        console.log(event.target);
+    handleChange = (e) => {
+        let tempData = this.state.loginData;
+        tempData[e.target.name] = e.target.value;
+        this.setState({
+            loginData: tempData
+        });
+    }
+
+    handleSubmit = (e) => {
+
+        axios.post('http://35.198.228.244:10000/auth/login', this.state.loginData)
+            .then((response) => {
+                localStorage.setItem('access_token', response.data.access_token);
+                axios.interceptors.request.use((config) => {
+                    const token = localStorage.getItem('access_token');
+                    config.headers.authorization = token;
+
+                    return config;
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            })
     }
 
     render() {
 
         return (
             <div>
-                <NavBar />
+                <NavBar mode='guest' userDatas=''/>
                 <div className='SignInSignUp-page'>
                     <div className='left-content'>
                         <span className='left-logo'><img src={logo} id="logo-img" alt="youngstar logo" /></span>
@@ -30,21 +55,21 @@ class SignIn extends React.Component {
                         <div className='right-content'>
                             <div className='form-name'>Sign In</div>
                             <div className='form-container'>
-                                <form onSubmit >
+                                <form >
 
                                     <div class="form-group">
                                         <label for="formGroupUsernameInput">Username</label>
-                                        <input type="text" class="form-control" id="formGroupUsernameInput" placeholder="Username" />
+                                        <input type="text" class="form-control" id="formGroupUsernameInput" name='username' onChange={this.handleChange} placeholder="Username" />
                                     </div>
 
                                     <div class="form-group">
                                         <label for="formGroupPasswordInput">Password</label>
-                                        <input type="password" class="form-control" id="formGroupPasswordInput" placeholder="Password" />
+                                        <input type="password" class="form-control" id="formGroupPasswordInput" name='password' onChange={this.handleChange} placeholder="Password" />
                                     </div>
 
-                                    <button type="submit" class="btn btn-success">Sign in</button>
+                                    <button type="submit" class="btn btn-success" onClick={this.handleSubmit}>Sign in</button>
 
-                                        <p>Don't have an account? <a href='/signup'>Create account</a></p>
+                                    <p>Don't have an account? <a href='/signup'>Create account</a></p>
                                 </form>
                             </div>
                         </div>
--- src/SignUp.js
+++ src/SignUp.js
@@ -2,20 +2,44 @@ import React from 'react';
 import NavBar from "./NavBar";
 import logo from './material/Logo.png';
 import './SignInSignUp.css';
+import axios from 'axios';
 
 class SignUp extends React.Component {
     
     constructor(props){
         super(props);
         this.state={
+            registerpData : { fullname:'', username:'', password:'' },
         };
+        this.handleChange.bind(this);
+        this.handleSubmit.bind(this);
+    }
+
+    handleChange = (e) => {
+        let tempData = this.state.registerData;
+        tempData[e.target.name] = e.target.value;
+        this.setState({
+            registerData: tempData
+        });
+        
+    }
+
+    handleSubmit = (e) => {
+
+        // axios.post('http://35.198.228.244:10000/users', this.state.registerData)
+        //     .then((response) => {
+        //         console.log(response);
+        //     })
+        //     .catch((error) => {
+        //         console.error(error);
+        //     })
     }
 
     render(){   
 
         return (
             <div>
-                <NavBar />
+                <NavBar mode='guest' userDatas=''/>
                 <div className='SignInSignUp-page'>
                     <div className='left-content'>
                         <span className='left-logo'><img src={logo} id="logo-img" alt="youngstar logo" /></span>
@@ -30,17 +54,17 @@ class SignUp extends React.Component {
 
                                     <div class="form-group">
                                         <label for="formGroupFullNameInput">Full name</label>
-                                        <input type="text" class="form-control" id="formGroupFullNameInput" placeholder="Full name" />
+                                        <input type="text" class="form-control" id="formGroupFullNameInput" name='fullname' onChange={this.handleChange} placeholder="Full name" />
                                     </div>
 
                                     <div class="form-group">
                                         <label for="formGroupUsernameInput">Username</label>
-                                        <input type="text" class="form-control" id="formGroupUsernameInput" placeholder="Username" />
+                                        <input type="text" class="form-control" id="formGroupUsernameInput" name='username' onChange={this.handleChange} placeholder="Username" />
                                     </div>
 
                                     <div class="form-group">
                                         <label for="formGroupPasswordInput">Password</label>
-                                        <input type="password" class="form-control" id="formGroupPasswordInput" placeholder="Password" />
+                                        <input type="password" class="form-control" id="formGroupPasswordInput" name='password' onChange={this.handleChange} placeholder="Password" />
                                     </div>
 
                                     <div class="form-group">
@@ -48,7 +72,7 @@ class SignUp extends React.Component {
                                         <input type="password" class="form-control" id="formGroupRepeatPasswordInput" placeholder="Repeat password" />
                                     </div>
 
-                                    <button type="submit" class="btn btn-success">Sign up</button>
+                                    <button type="submit" class="btn btn-success" onClick={this.handleSubmit}>Sign up</button>
 
                                         <p>Already have an account? <a href='/signin'>Sign in</a></p>
                                 </form>
