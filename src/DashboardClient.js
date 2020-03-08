b0b33bd684168be0cade770dede9bd4b23f78914
Itthithee <poomlong@gmail.com>
2020-03-09 04:23:03 +0700

    design layout by using grid

--- src/DashboardClient.js
+++ src/DashboardClient.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import NavBar from "./NavBar";
+import "./DashBoardClient.css"
+import { Table, Container, Row, Col } from 'react-bootstrap';
+//import { ReactComponent } from '*.svg';
 // import logo from './material/Logo.png';
 
 class DashboardClient extends React.Component {
@@ -7,6 +10,7 @@ class DashboardClient extends React.Component {
     constructor(props){
         super(props);
         this.state={
+            mode : "client"
         };
     }
 
@@ -14,11 +18,76 @@ class DashboardClient extends React.Component {
 
         return (
             <div>
-                <NavBar />
-                Dashboard
+                <NavBar mode={this.state.mode} userDatas={""} />
+                <Container>
+                    <header></header>
+                    <Row>
+                        <Col sm={8}>
+                            <FreelancerBox/>
+                        </Col>
+                        <Col sm={4}>
+                        
+                        </Col>
+                    </Row>
+                    <Row>
+                        Timeline
+                    </Row>
+                </Container>
+                
             </div>
         );
     }   
 }
 
+class FreelancerBox extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            freelancerList : [{userId : "1", fname : "Irma", lname : "Williamson", score : 10},]
+        }
+        this.showInterestedList = this.showInterestedList.bind(this);
+    }
+
+    
+
+    showInterestedList(){
+        return this.state.freelancerList.length === 0 ? "No one interested yet" : this.state.freelancerList.map(item => 
+            <tr key={item.userId}>
+                <td>{item.fname}</td>
+                <td>{item.lname}</td>
+                <td>
+                    <button type="button" className="btn btn-secondary btn-block" onClick={""}>
+                    Delete </button>
+                </td>
+            </tr>
+        )
+    }
+
+    render(){
+        return (
+            <div className="freelancerBox">
+                <header>Interested Freelancer</header>
+                <Table responsive><tbody>{this.showInterestedList()}</tbody></Table>
+            </div>
+        )
+    }
+}
+
+class StatusBox extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+}
+
+class ContractBox extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+}
 export default DashboardClient;
