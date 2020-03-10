b2e7bb94e08e5fe858816aa3f6c5001834f8acea
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-03-10 19:14:56 +0700

    revert timeline to normal ugly :p

--- src/DashboardComponent.js
+++ src/DashboardComponent.js
@@ -171,114 +171,114 @@ export class DashboardTimeline extends React.Component {
 
     getTimelineComponent() {
         return (
-            // <Table className="component-timeline" responsive="sm" hover>
-            //     <thead>
-            //         <tr>
-            //             <th>Status</th>
-            //             <th>Date & Time</th>
-            //         </tr>
-            //     </thead>
-            //     <tbody>
-            //         {this.getTimelineBody()}
-            //         {/* <tr key='timeline-open'>
-            //             <td><h2><Badge pill variant='success'>Open</Badge></h2></td>
-            //             <td>{}</td>
-            //         </tr> */}
-            //     </tbody>
-            // </Table>
+            <Table className="component-timeline" responsive="sm" hover>
+                <thead>
+                    <tr>
+                        <th>Status</th>
+                        <th>Date & Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.getTimelineBody()}
+                    {/* <tr key='timeline-open'>
+                        <td><h2><Badge pill variant='success'>Open</Badge></h2></td>
+                        <td>{}</td>
+                    </tr> */}
+                </tbody>
+            </Table>
 
-            <VerticalTimeline >
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                    date="2011 - present"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    // icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                    <p>
-                        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2010 - 2011"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    // icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Art Director</h3>
-                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                    <p>
-                        Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2008 - 2010"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    // icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-                    <p>
-                        User Experience, Visual Design
-    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2006 - 2008"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    // icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                    <p>
-                        User Experience, Visual Design
-    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="April 2013"
-                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    // icon={<SchoolIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-                    <p>
-                        Strategy, Social Media
-    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="November 2012"
-                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    // icon={<SchoolIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-                    <p>
-                        Creative Direction, User Experience, Visual Design
-    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="2002 - 2006"
-                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    // icon={<SchoolIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-                    <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-                    <p>
-                        Creative Direction, Visual Design
-    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-                    // icon={<StarIcon />}
-                />
-            </VerticalTimeline>
+    //         <VerticalTimeline >
+    //             <VerticalTimelineElement
+    //                 className="vertical-timeline-element--work"
+    //                 contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    //                 contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+    //                 date="2011 - present"
+    //                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    //                 // icon={<WorkIcon />}
+    //             >
+    //                 <h3 className="vertical-timeline-element-title">Creative Director</h3>
+    //                 <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+    //                 <p>
+    //                     Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+    // </p>
+    //             </VerticalTimelineElement>
+    //             <VerticalTimelineElement
+    //                 className="vertical-timeline-element--work"
+    //                 date="2010 - 2011"
+    //                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    //                 // icon={<WorkIcon />}
+    //             >
+    //                 <h3 className="vertical-timeline-element-title">Art Director</h3>
+    //                 <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+    //                 <p>
+    //                     Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+    // </p>
+    //             </VerticalTimelineElement>
+    //             <VerticalTimelineElement
+    //                 className="vertical-timeline-element--work"
+    //                 date="2008 - 2010"
+    //                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    //                 // icon={<WorkIcon />}
+    //             >
+    //                 <h3 className="vertical-timeline-element-title">Web Designer</h3>
+    //                 <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+    //                 <p>
+    //                     User Experience, Visual Design
+    // </p>
+    //             </VerticalTimelineElement>
+    //             <VerticalTimelineElement
+    //                 className="vertical-timeline-element--work"
+    //                 date="2006 - 2008"
+    //                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    //                 // icon={<WorkIcon />}
+    //             >
+    //                 <h3 className="vertical-timeline-element-title">Web Designer</h3>
+    //                 <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+    //                 <p>
+    //                     User Experience, Visual Design
+    // </p>
+    //             </VerticalTimelineElement>
+    //             <VerticalTimelineElement
+    //                 className="vertical-timeline-element--education"
+    //                 date="April 2013"
+    //                 iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    //                 // icon={<SchoolIcon />}
+    //             >
+    //                 <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+    //                 <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+    //                 <p>
+    //                     Strategy, Social Media
+    // </p>
+    //             </VerticalTimelineElement>
+    //             <VerticalTimelineElement
+    //                 className="vertical-timeline-element--education"
+    //                 date="November 2012"
+    //                 iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    //                 // icon={<SchoolIcon />}
+    //             >
+    //                 <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+    //                 <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+    //                 <p>
+    //                     Creative Direction, User Experience, Visual Design
+    // </p>
+    //             </VerticalTimelineElement>
+    //             <VerticalTimelineElement
+    //                 className="vertical-timeline-element--education"
+    //                 date="2002 - 2006"
+    //                 iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    //                 // icon={<SchoolIcon />}
+    //             >
+    //                 <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+    //                 <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+    //                 <p>
+    //                     Creative Direction, Visual Design
+    // </p>
+    //             </VerticalTimelineElement>
+    //             <VerticalTimelineElement
+    //                 iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+    //                 // icon={<StarIcon />}
+    //             />
+    //         </VerticalTimeline>
 
         );
     }
