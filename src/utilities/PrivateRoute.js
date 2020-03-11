5620dbac1bdb0862aba31b1ebbfe87a9dbc3655a
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-03-11 16:50:25 +0700

    fix PrivateRoute

--- src/utilities/PrivateRoute.js
+++ src/utilities/PrivateRoute.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import axios from 'axios';
 import LocalStorageService from "../LocalStorageService";
-var utilities = require('../Utilities.json');
 
 function PrivateRoute({ component: Component, ...rest }) {
 
@@ -10,7 +8,7 @@ function PrivateRoute({ component: Component, ...rest }) {
         <Route
             {...rest}
             render={props =>
-                auth() ? (
+                LocalStorageService.getAccessToken() ? (
                     <Component {...props} />
                 ) : (
                         <Redirect to="/signin" />
@@ -18,28 +16,6 @@ function PrivateRoute({ component: Component, ...rest }) {
             }
         />
     );
-}
-
-function auth() {
-    let returnBool = false;
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + LocalStorageService.getAccessToken();
-    axios.get(utilities['backend-url'] + "/users/fromtoken")
-        .then(res => {
-            returnBool = true;
-        }).catch((error) => {
-
-            if (error.response.status === 401) {
-                console.log('Unauthorization');
-                returnBool = false;
-
-            } else {
-                console.error(error);
-                returnBool = false;
-            }
-        });
-
-    console.log(returnBool);
-    return returnBool;
 }
 
 export default PrivateRoute;
