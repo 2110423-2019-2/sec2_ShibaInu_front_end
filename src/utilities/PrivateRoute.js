a5a207e404e11d02736344172042d7dc7592e38e
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-03-11 15:24:02 +0700

    create PrivateRoute

---
+++ src/utilities/PrivateRoute.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+var utilities = require('../Utilities.json');
+
+function PrivateRoute({ component: Component, ...rest }) {
+
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                auth() ? (
+                    <Component {...props} />
+                ) : (
+                        <Redirect to="/signin" />
+                    )
+            }
+        />
+    );
+}
+
+function auth() {
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + LocalStorageService.getAccessToken();
+    axios.get(utilities['backend-url'] + "/users/fromtoken")
+        .then(res => {
+            return true;
+        }).catch((error) => {
+
+            if (error.response.status === 401) {
+                console.log('Unauthorization');
+                return false;
+
+            } else {
+                console.error(error);
+                return false;
+            }
+        });
+}
+
+export default PrivateRoute;
