340012e0a90043d5116f754789cba55a1acdeed7
Itthithee <poomlong@gmail.com>
2020-03-26 04:54:15 +0700

    add new css and resizeImage function

--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,43 +1,104 @@
 import React from 'react';
-
+import {Spinner,Badge} from 'react-bootstrap';
+import swal from 'sweetalert'
 class ImageUploader extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             selectedImage : null,
-            imageUrl : null
+            imageUrl : null,
+            onloadUpload : false,
+            uploadstate : this.props.upload,
+            maxHeight:1920,
+            maxWidth:1080,
         }
         this.selectHandler = this.selectHandler.bind(this,);
+        this.uploadHandler = this.uploadHandler.bind(this);
+        this.resizeImage = this.resizeImage.bind(this);
     }
     selectHandler(event){
+        console.log("do")
         if(event.target.files[0]){
             let image = event.target.files[0];
-            this.setState({selectedImage : image},console.log(this.state.selectedImage))
             let reader = new FileReader();
-            let url = reader.readAsDataURL(image);
-            reader.onloadend =  (e) => {
-                this.setState({
-                    imageUrl: reader.result
-                })
-              };
-            console.log(url)
+            this.setState({imageUrl:null})
+            reader.onloadstart = () => {
+                this.setState({onloadUpload : true})
+            }
+            reader.onload =  () => {
+                let img = new Image();
+                img.src = reader.result;
+                img.onload = ()=>{
+                    let resizedImageUrl = this.resizeImage(img,this.state.maxHeight,this.state.maxWidth,1.0);
+                    this.setState({
+                        imageUrl: resizedImageUrl
+                    })
+                }
+                this.setState({selectedImage : image},console.log(image))
+                
+            }
+            reader.onloadend = ()=>{
+                this.setState({onloadUpload:false})
+            }
+            reader.readAsDataURL(image);
+            
+        }
+        
+    }
+    resizeImage(image, maxWidth, maxHeight, quality) {
+        var canvas = document.createElement('canvas');
+    
+        var width = image.width;
+        var height = image.height;
+    
+        if (width > height) {
+            if (width > maxWidth) {
+                height = Math.round(height * maxWidth / width);
+                width = maxWidth;
+            }
+        } else {
+            if (height > maxHeight) {
+                width = Math.round(width * maxHeight / height);
+                height = maxHeight;
+            }
+        }
+    
+        canvas.width = width;
+        canvas.height = height;
+    
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(image, 0, 0, width, height);
+        return canvas.toDataURL("image/*", quality);
+    }
+    componentDidUpdate(prevProps){
+        if(this.props.upload !== prevProps.upload){
+            this.setState({uploadstate : this.props.upload})
         }
     }
     uploadHandler(){
-        
+        console.log("Uploaded");
+        if(this.state.selectedImage === null){
+            swal("Error","Image file cannot be null","error");
+            return;
+        }
+        this.setState({uploadstate : false,selectedImage:null,imageUrl:null})
     }
     render(){
-
+        if(this.state.uploadstate){
+            this.uploadHandler()
+        }
         return(
         <div className="image-uploader">
-            <label>
-                <input type="file" onChange={this.selectHandler} hidden="true"/>
-                choose
-            </label>
+            
             <div className="image-container">
-                {this.state.imageUrl===null?"put sth": <img src={this.state.imageUrl} alt="profile" width="200" height="200"/>}
+                {this.state.onloadUpload?<Spinner animation="border" />:null}
+                {this.state.imageUrl===null? null: <img src={this.state.imageUrl} alt="profile" width="200" height="200"/>}
             </div>
-            <button>upload</button>
+            <label>
+                <input type="file" onChange={this.selectHandler} value={this.state.image} accept="image/*" hidden={true}/>
+                <Badge variant={this.state.imageUrl===null?"secondary":"primary"}>choose</Badge>
+            </label>{' '}
+            {this.state.selectedImage!==null?this.state.selectedImage.name:"no file choosen"}
         </div>
         )
     }
