862f79278dd040c61ef93b810fd45a3f940d8a0c
Itthithee <poomlong@gmail.com>
2020-04-01 11:14:06 +0700

    a contract's template

---
+++ src/contractExample.js
@@ -0,0 +1,40 @@
+const example = ` <p><strong>Computer Service Agreement</strong></p>
+
+<p>This agreement is made and entered into this&nbsp;<strong>{date}</strong>&nbsp;day of&nbsp;<strong>{month}</strong>,&nbsp;<strong>{year}</strong>, between&nbsp;<strong>{Name}</strong>, henceforth known as &quot;Provider,&quot; and&nbsp;<strong>{Name}</strong>, henceforth known as &quot;Recipient.&quot;</p>
+
+<p>Starting on&nbsp;<strong>{date}</strong>, the Provider will provide the following services for&nbsp;<strong>{Client Account #}</strong>&nbsp;and&nbsp;<strong>{Service #}</strong>:&nbsp;<strong>{Description of services}</strong>. The agreement will terminate upon the completion of said services.</p>
+
+<p>The Recipient will pay the Provider according to the following payment plan:&nbsp;<strong>{$/hour}</strong>,&nbsp;<strong>{$ for labor}</strong>,&nbsp;<strong>{$ for parts}</strong>, etc. Incidental service and expenses fees may apply and will be billed to the Recipient at a basis of&nbsp;<strong>{rate}</strong>.</p>
+
+<p>Repairs that fall under warranty will not be charged to the Recipient. Recipient understands that some damages are not covered by warranty. Both Provider and Recipient will refer to warranty terms and agree upon the services that are to be provided.</p>
+
+<p>The Provider shall repair or replace any outdated, missing or damaged subassemblies or components. The replacements shall be of similar or better quality than those that are being replaced.</p>
+
+<p>The Provider shall keep the computer in good repair and be responsible, at no cost to the Recipient, for further or unrelated damages caused by the Provider. The Provider shall not be responsible for any unrelated viruses or damages that were inflicted upon the computer prior to the agreement.</p>
+
+<p>The Recipient shall be responsible for backing up data from the computer. The Recipient shall hold harmless the Provider for any accidental data loss.</p>
+
+<p>Should the repairs take place on the Recipient&#39;s property, the Provider will ensure that the property is returned in the state it was in prior to the repairs.</p>
+
+<p>The Provider attests that he/she has all the certifications and licenses required by law.</p>
+
+<p>Any violation of the aforementioned terms will be considered a breach of contract. The agreement is listed above in its entirety.</p>
+
+<p>____________________________<br />
+Provider Signature</p>
+
+<p>&nbsp;</p>
+
+<p>____________________________<br />
+Date</p>
+
+<p>&nbsp;</p>
+
+<p>____________________________<br />
+Recipient Signature</p>
+
+<p>&nbsp;</p>
+
+<p>____________________________<br /> 
+Date</p> `
+export default example
