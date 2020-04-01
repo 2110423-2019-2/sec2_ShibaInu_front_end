327e6ada83d7ea0a0469fe164e5677474ac4b1da
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-04-01 22:36:32 +0700

    change in Navbar word from My Payment to My payment

--- src/NavBar.js
+++ src/NavBar.js
@@ -176,7 +176,7 @@ class NavBar extends React.Component {
           My job
         </DropdownItem>
         <DropdownItem href="/payment" id="dropdown-item-balance" className="color-black">
-          My Payment
+          My payment
         </DropdownItem>
         <DropdownItem href="/chat" id="dropdown-item-chat" className="color-black">
           Chat
