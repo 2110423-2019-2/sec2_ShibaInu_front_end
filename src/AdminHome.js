343d58a884c0ff09ff58200f8f5683fe4de91335
Itthithee <poomlong@gmail.com>
2020-04-22 02:39:03 +0700

    make admin can view image through modal by clicking view

--- src/AdminHome.js
+++ src/AdminHome.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './AdminHome.css';
-import { Container, Row, Col, Table } from "react-bootstrap";
+import { Container, Row, Col, Table, Modal, Spinner } from "react-bootstrap";
 import axios from "axios";
 import swal from "sweetalert";
 import LocalStorageService from "./LocalStorageService";
@@ -12,7 +12,9 @@ class AdminHome extends React.Component {
       isDataLoad: false,
       userDatas: {},
       isUserDataLoad: false,
-      banned: []
+      banned: [],
+      showModal : false,
+      modalData : {userId : "", mode : ""}
     };
   }
 
@@ -154,17 +156,42 @@ class AdminHome extends React.Component {
       </tr>
     );
   }
+  
+  setShowModalData = async(userId,mode) =>{
+    await this.setState({
+      modalData : { userId : userId, mode : mode}
+    })
+    this.showHideModal(true);
+  }
+  showHideModal = (show=false)=>{
+    this.setState({
+      showModal : show,
+    })
+    return show
+  }
 
   detailTable = () => {
     return this.state.userDatas.map((user,index)=>(
       <tr key={index} className="text-center">
       <td className="align-middle">
-        {user.firstName}
+        {user.firstName}{' '}
         {user.lastName}
       </td>
       <td className="align-middle">{user.identificationNumber}</td>
-      <td className="align-middle">{user.identificationCardWithFacePic}</td>
-      <td className="align-middle">{user.identificationCardPic}</td>
+      <td className="align-middle">
+        {user.identificationCardWithFacePic !== null ?
+         <h5 id="modal-view-link" onClick={()=>this.setShowModalData(user.userId,"selfie")}>view</h5>
+         :
+          null
+        }
+      </td>
+      <td className="align-middle">
+        {user.identificationCardPic !== null ?
+        <h5 id="modal-view-link" onClick={()=>this.setShowModalData(user.userId,"idcard")}>view</h5>
+        :
+         null  
+        }
+        </td>
       <td className="align-middle">
         <button type="button" className="btn btn-secondary btn-block" onClick={()=>this.disapproveHandler(index)}>
           Disapprove
@@ -197,6 +224,7 @@ class AdminHome extends React.Component {
                 <thead>{this.headTable()}</thead>
                 <tbody>{this.detailTable()}</tbody>
               </Table>
+              <ImageModal open={this.state.showModal} modalData={this.state.modalData} callback={this.showHideModal}/>
             </Col>
           </Row>
         </Container>
@@ -205,4 +233,108 @@ class AdminHome extends React.Component {
   }
 }
 
+class ImageModal extends React.Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      show:false,
+      modalData : {userId : "", mode :""},
+      image : "",
+      isFetching : true,
+      modalTitle : "",
+    }
+  }
+  fetchIDCard = ()=>{
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + LocalStorageService.getAccessToken();
+    axios.get(utilities['backend-url'] + "/users/IDCard/"+this.state.modalData.userId,{ responseType: 'blob' })
+    .then(res => {
+      let blob =  URL.createObjectURL(res.data)
+      console.log(blob,"1")
+      this.setState({ 
+        image : blob,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+    .finally(()=>{
+      this.setState({
+        isFetching : false
+      })
+    })
+    
+  }
+  fetchSelfieImage = ()=>{
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + LocalStorageService.getAccessToken();
+    axios.get(utilities['backend-url'] + "/users/IDCardWithFace/"+this.state.modalData.userId,{ responseType: 'blob' })
+    .then(res => {
+        let blob = URL.createObjectURL(res.data)
+        this.setState({ 
+          image : blob,
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+    .finally(()=>{
+      this.setState({
+        isFetching : false
+      })
+    })
+  }
+  handleClose = ()=>{
+    this.state.closeFunction();
+    URL.revokeObjectURL(this.state.image)
+  }
+  onEnter = ()=>{
+    this.setState({image : "",isFetching : true})
+  }
+  componentDidUpdate = async(prevProps)=>{
+    if(prevProps!==this.props){
+      await this.setState({
+        show : this.props.open,
+        modalData : this.props.modalData,
+        closeFunction : this.props.callback,
+      })
+      if(this.state.modalData.mode === "idcard" && this.state.show && this.state.image === ""){
+        this.setState({modalTitle : "National ID Card Image"})
+        await this.fetchIDCard()
+      }else if(this.state.modalData.mode === "selfie" && this.state.show && this.state.image === ""){
+        this.setState({modalTitle : "Selfie Image"})
+        await this.fetchSelfieImage()
+      }
+    }
+  }
+  renderLoading() {
+    return (<Spinner animation="border" role="status" className="loading">
+        <span className="sr-only">Loading...</span>
+    </Spinner>);
+  }
+  render(){
+    if(this.state.userId=== ""){
+      return null;
+    }
+    return (
+      <>
+        <Modal dialogClassName="image-modal" show={this.props.open} onHide={this.handleClose} onEnter={this.onEnter}>
+          <Modal.Header className="modalHead" closeButton>
+            <Modal.Title>{this.state.modalTitle}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body >
+            <Container>
+            {
+               this.state.isFetching?this.renderLoading():<a href={this.state.image}><img src={this.state.image} alt={"image-"+this.state.mode}/></a>
+            }
+            </Container>
+          </Modal.Body>
+          <Modal.Footer>
+            <button className="outline-success" onClick={this.handleClose}>
+              close
+            </button>
+          </Modal.Footer>
+        </Modal>
+        </>
+    )
+  }
+}
 export default AdminHome;
