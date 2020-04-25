e2a992de89e3fc8158d32a787386ce1e692d1173
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-04-26 02:25:50 +0700

    add style and make money get from db in HomeClient

--- src/HomeClient.js
+++ src/HomeClient.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./HomeClient.css";
 import "./HomeFreelancer.css";
-import { Container, Row, Col, Table } from "react-bootstrap";
+import { Container, Row, Col, Table, Badge } from "react-bootstrap";
 import axios from "axios";
 import LocalStorageService from './LocalStorageService';
 import { FaBullhorn, FaInfoCircle } from "react-icons/fa";
@@ -15,34 +15,51 @@ class Home extends React.Component {
       isJobDataLoad: false,
       mode: "client",
       announce: "",
+      sumMoney: 0,
       userID: LocalStorageService.getUserID(),
       isAnnounceLoad: false,
+      badgeStyle: {
+        open: "success",
+        accepted: "primary",
+        working: "warning",
+        done: "primary",
+        closed: "danger"
+      }
     };
   }
 
-  fetchDatas = () => {
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + LocalStorageService.getAccessToken();
-    axios
-      .get(process.env.REACT_APP_BACKEND_URL + "/users/" + this.state.userID)
-      .then(res => {
-        const userDatas = res.data;
-        this.setState({ userDatas: userDatas, isUserDataLoad: true });
-        console.log(this.state.userDatas);
-      });
-    axios
-      .get(process.env.REACT_APP_BACKEND_URL + "/jobs/recent/" + this.state.userID)
-      .then(res => {
-        const jobDatas = res.data;
-        this.setState({ jobDatas: jobDatas, isJobDataLoad: true });
-        console.log(this.state.jobDatas);
-      });
-    axios
-      .get(process.env.REACT_APP_BACKEND_URL + "/announcement")
-      .then(res => {
-        const announce = res.data;
-        this.setState({ announce: announce, isAnnounceLoad:true });
-        console.log(this.state.announce);
-      });
+  fetchDatas = async () => {
+    try {
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + LocalStorageService.getAccessToken();
+      await axios
+        .get(process.env.REACT_APP_BACKEND_URL + "/payment/sum")
+        .then(res => {
+          this.setState({ sumMoney: -1 * Number(res.data.sum) });
+        });
+      await axios
+        .get(process.env.REACT_APP_BACKEND_URL + "/users/" + this.state.userID)
+        .then(res => {
+          const userDatas = res.data;
+          this.setState({ userDatas: userDatas, isUserDataLoad: true });
+          console.log(this.state.userDatas);
+        });
+      await axios
+        .get(process.env.REACT_APP_BACKEND_URL + "/jobs/recent/" + this.state.userID)
+        .then(res => {
+          const jobDatas = res.data;
+          this.setState({ jobDatas: jobDatas, isJobDataLoad: true });
+          console.log(this.state.jobDatas);
+        });
+      await axios
+        .get(process.env.REACT_APP_BACKEND_URL + "/announcement")
+        .then(res => {
+          const announce = res.data;
+          this.setState({ announce: announce, isAnnounceLoad: true });
+          console.log(this.state.announce);
+        });
+    } catch (error) {
+      console.error(error);
+    };
   };
 
   componentDidMount = () => {
@@ -62,8 +79,7 @@ class Home extends React.Component {
         <td className="align-middle">
           {job.name}
         </td>
-        <td className="align-middle">-</td>
-        <td className="align-middle">{job.status}</td>
+        <td className="align-middle"><h5><Badge variant={this.state.badgeStyle[job.status]}>{job.status}</Badge></h5></td>
         <td className="align-middle">
           <button type="button" className="btn btn-secondary btn-block" id={job.jobId} onClick={this.handleClickJobDetail.bind(this)}>
             Detail
@@ -77,18 +93,15 @@ class Home extends React.Component {
           <h5>Name</h5>
         </td>
         <td>
-          <h5>Freelancer</h5>
-        </td>
-        <td>
           <h5>Status</h5>
         </td>
         <td></td>
       </tr>
     );
-    
+
     var announce;
-    if(this.state.isAnnounceLoad){
-      announce = this.state.announce.map((announce)=>(
+    if (this.state.isAnnounceLoad) {
+      announce = this.state.announce.map((announce) => (
         <Row>
           <Col className="bg-light shadow pt-2">
             <h5 className="announce-title"><FaBullhorn /> {announce.title}</h5>
@@ -132,13 +145,8 @@ class Home extends React.Component {
               <div className="rounded shadow bg-light">
                 <Container fluid={true}>
                   <Row>
-                    <Col xs={8}>
-                      <h5>Account</h5>
-                      <br />
-                      <h5>Balance</h5>
-                    </Col>
                     <Col>
-                      <h5>{this.state.userDatas.money} USD</h5>
+                      <h3>Total {this.state.sumMoney}฿</h3>
                     </Col>
                   </Row>
                 </Container>
