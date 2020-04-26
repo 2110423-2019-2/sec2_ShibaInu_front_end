d1a8ad2c5ca8946f2b5fe3a836c14e17f1924c0f
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-04-26 17:16:08 +0700

    move firebase credential to env

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDCuuLgHsxXkrSh6ojklFw6zDkKaDPnYMY",
-    authDomain: "se-project-268809.firebaseapp.com",
-    databaseURL: "https://se-project-268809.firebaseio.com",
-    projectId: "se-project-268809",
-    storageBucket: "se-project-268809.appspot.com",
-    messagingSenderId: "692247152850",
-    appId: "1:692247152850:web:6b2e6e6b9b432f368b050b"
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID
 };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
